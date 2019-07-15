3499d25ed9db8afaca883ac44742afc14c674fc8
Temirlan24 <52044447+Temirlan24@users.noreply.github.com>
2019-07-15 16:12:14 +0600

    Практическая работа try. Создал класс и использовал метод внутри метода

---
+++ PracticaExceptions/PracticaExceptions/ClassExceptionsWithMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaExceptions
+{
+    class ClassExceptionsWithMethods
+    {
+        public void ChekArray(int[] array, int length)
+        {
+            for(int i = 0; i < length; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+        }
+        public void FillArray(int elementLength,int length)
+        {
+            int[] array = new int[elementLength];
+            Random random = new Random();
+            for(int i = 0; i < elementLength; i++)
+            {
+                array[i] = random.Next(1, 20);
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+            ChekArray(array, length);
+        }
+    }
+}
---
+++ PracticaExceptions/PracticaExceptions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassExceptionsWithMethods exception = new ClassExceptionsWithMethods();
+            try
+            {
+                exception.FillArray(5, 7);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("\nВыход за границу массива!!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
